19027a62e1b6dba182d3276b7936fa35451ddf7d
BaskLash <olivier.luethy@gmx.net>
2026-03-25 21:06:32 +0100

    update short cut icon image + robots.txt baseURL to PROD

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,15 +42,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
+        url: "/icon128.png",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "/icon128.png",
         type: "image/svg+xml",
       },
     ],
